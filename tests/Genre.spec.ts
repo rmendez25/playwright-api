6ecb71528a78d572a549b07f84659d74381fa47c
Robert Mendez <rmendezfamilia@gmail.com>
2026-03-16 17:26:40 -0400

    create delete test and refactoring

--- tests/Genre.spec.ts
+++ tests/Genre.spec.ts
@@ -1,5 +1,13 @@
 import { test, expect } from '../fixtures/baseTest';
 
+const invalidId = 198929;
+ let body: any = null;
+
+test.beforeAll(async ({ genres }) => {
+    const genre = await genres.create({ name: 'Comedy' });
+    body = await genre.json();
+})
+
 test.describe('Create Genre scenarios', () => {
     test('Create Genre Happy Path', async ({ genres }) => {
         const genre = await genres.create({ name: 'Action' });
@@ -17,13 +25,6 @@ test.describe('Create Genre scenarios', () => {
 })
 
 test.describe('Get Genres test Cases', () => {
-    let body: any = null;
-
-    test.beforeEach(async ({ genres }) => {
-        const genre = await genres.create({ name: 'Comedy' });
-        body = await genre.json();
-    })
-
     test('Get Genre By valid ID', async ({ genres }) => {
         const byId = await genres.getById(body.id);
         const bodyById = await byId.json();
@@ -31,7 +32,6 @@ test.describe('Get Genres test Cases', () => {
     })
 
     test('Get Genre By invalid ID', async ({ genres }) => {
-        const invalidId = 198929;
         const byId = await genres.getById(invalidId);
         const bodyById = await byId.json();
         expect(bodyById.error).toBe('Not Found');
@@ -47,13 +47,6 @@ test.describe('Get Genres test Cases', () => {
 
 test.describe('Update Genres Scenararios', () => {
 
-    let body: any = null;
-
-    test.beforeEach(async ({ genres }) => {
-        const genre = await genres.create({ name: 'Comedy' });
-        body = await genre.json();
-    })
-
     test('Update Genre Happy Path', async ({ genres }) => {
         const updatedGenre = await genres.update(body.id, { name: "Genre Updated" });
         const response = await updatedGenre.json();
@@ -62,7 +55,6 @@ test.describe('Update Genres Scenararios', () => {
     })
 
     test('Update Genre with invalid ID', async ({ genres }) => {
-        const invalidId = 198929;
         const updatedGenre = await genres.update(invalidId, { name: "Invalid update" })
         const response = await updatedGenre.json();
         expect(updatedGenre.status()).toBe(404);
@@ -78,4 +70,21 @@ test.describe('Update Genres Scenararios', () => {
         expect(response.name).toBe('The genre name cannot be empty or null.');
     })
 
+})
+
+test.describe('Delete Genres Scenararios', () => {
+    test('Delete Genre Happy Path', async ({ genres }) => {
+        const deleteGenre = await genres.delete(body.id);
+        const response = await deleteGenre.json();
+        expect(deleteGenre.status()).toBe(200);
+        expect(response.message).toBe('Genre deleted successfully!');
+    })
+
+    test('Delete Genre with invalid ID', async ({ genres }) => {
+        const deleteGenre = await genres.delete(invalidId)
+        const response = await deleteGenre.json();
+        expect(deleteGenre.status()).toBe(404);
+        expect(response.error).toBe('Not Found');
+        expect(response.message).toBe(`Genre with the ID: ${invalidId} not found!`);
+    })
 })
